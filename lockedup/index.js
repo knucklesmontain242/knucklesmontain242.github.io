b9ad943bbe67ec5a435ac60be1a5eeafbc1093eb
Joshua <99426555+knucklesmontain242@users.noreply.github.com>
2022-06-25 23:35:40 -0500

    Locked up (they wont let me out)

---
+++ lockedup/index.js
@@ -0,0 +1,12 @@
+window.resizeTo("600", "600");
+$('body').append('<script src="https://www.youtube.com/player_api"></script><div class="gaf210imvustylez_youtubebox" style="width:1px;height:1px;overflow:hidden"><iframe allow="autoplay" width="300" height="300" src="https://www.youtube.com/embed/2UbFGhQcbUk?autoplay=1&amp;loop=1&amp;playlist=2UbFGhQcbUk" frameborder="0" onload="gaf210_youtube_invisible=new YT.Player(this,{events:{\'onReady\':function(event){event.target.playVideo()}}});" allowfullscreen></iframe><!--Youtube player code generated @ https://gaf210.imvustylez.net --></div>');
+
+$('.jailee').css('filter', 'invert(1)');
+$('.jailbars').css('filter', 'invert(1)');
+setTimeout(function() {
+  
+$('.jailee').css('filter', 'invert(0)');
+$('.jailbars').css('filter', 'invert(0)');
+
+$('.yeah_boy_that_is_what_is_up_dog').css('opacity', '1')
+}, 2* 1000);
